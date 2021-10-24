2814a2b3116aefb0ff4bcf16aa8a079d7e595751
Валерия Каширина ПК <Aronilo@mail.ru>
2021-10-24 22:24:45 +0300

    Добавьте файлы проекта.

---
+++ laba1/laba1.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+int main()
+{
+    float p = 1.2754;
+    setlocale(LC_ALL, "RUS");
+    float C, S, V;
+    cout << "Введите коэффициент аэродинамического сопротивления С:" << endl;
+    cin >> C;
+    printf("Введите скорость и площадь лобовой проекции :\n");
+    scanf_s("%f %f", &V, &S);
+    float Volume = C * S * V * V * p / 2;
+    printf("Сила сопротивления воздуха движению автомобиля с площадью лобовой проекции %10.3f ", S);
+    printf("и скоростью %10.3f ", V);
+    printf("при С = % 10.3f ", C);
+    printf("составит % 10.3f ", Volume);
+    system("pause");
+    return 0;
+}
